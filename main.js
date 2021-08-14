cdcdd482c636e800e0d84822a6e0ece8d9a7e285
AdrielDatario <79733895+AdrielDatario@users.noreply.github.com>
2021-08-13 18:34:45 -0700

    save, a tag work, download pic

--- main.js
+++ main.js
@@ -12,20 +12,30 @@ recognition.onresult = function(event){
 
     var Content = event.results[0][0].transcript;
     console.log(Content);
+    if(Content =="take my selfie"){
+        console.log("taking selfie ---");
+        speak();
+    }
 
-    document.getElementById("textbox").innerHTML = Content;
-    speak();
+    //document.getElementById("textbox").innerHTML = Content;
+    //speak();
 }
 
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your Selfie in 5 sec"
+    //document.getElementById("textbox").value;
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    }, 5000);
 }
 
 Webcam.set({
@@ -36,3 +46,16 @@ Webcam.set({
 });
 
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
